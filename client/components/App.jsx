786f518f04918f2ed0219483eecc06edd8915763
Tom Clayton <tgclayton1@gmail.com>
2020-05-24 12:55:15 +1200

    create state and props of App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,17 +6,35 @@ import Instructions from './Instructions'
 import LoadStart from './LoadStart'
 
 class App extends Component {
-  render () {
-    return (
-      <Router>
-        <h1 id = 'main-title'>The Game of Life</h1>
-        <Route exact path = '/' component = {() => <Home/>} />
-        <Route exact path = '/game' component = {() => <GameView/>} />
-        <Route exact path = '/instructions' component = {() => <Instructions/>} />
-        <Route exact path = '/load' component = {() => <LoadStart/>} />
-      </Router>
-    )
+  constructor () {
+    super()
+    this.state = {
+      mapArr: null,
+      generations: null
+    }
+    this.setMap = this.setMap.bind(this)
   }
+
+setMap = (mapArr) => {
+  this.setState({
+    mapArr: mapArr
+  })
+}
+
+render () {
+  return (
+    <Router>
+      <h1 id = 'main-title'>The Game of Life</h1>
+      <Route exact path = '/' component = {() => <Home/>} />
+      <Route exact path = '/game' component = {() => <GameView
+        setMap = {this.setMap}
+        mapArr ={this.state.mapArr}
+        gen = {this.state.generations}/>} />
+      <Route exact path = '/instructions' component = {() => <Instructions/>} />
+      <Route exact path = '/load' component = {() => <LoadStart/>} />
+    </Router>
+  )
+}
 }
 
 export default App
