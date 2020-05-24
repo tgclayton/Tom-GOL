9541f4d848026f5aa704c638da8e94e81fb44e9a
Tom Clayton <tgclayton1@gmail.com>
2020-05-24 13:20:48 +1200

    makerandommap creates map array

--- client/components/GameView.jsx
+++ client/components/GameView.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import Tile from './Tile'
 
+const makeRandomMap = () => {
+  const field = new Array(400).fill(0)
+  let newField = []
+  newField = field.map(tile => {
+    let rand = Math.random()
+    if (rand > 0.6) {
+      tile = 1
+    }
+    return tile
+  })
+  return newField
+}
+
 const GameView = (props) => {
   return (
+    <>
     <div id='tile-container' className = ''>
       {props.mapArr.map(tile => {
         return (
@@ -10,6 +24,8 @@ const GameView = (props) => {
         )
       })}
     </div>
+    <button onClick = {makeRandomMap}>Create Random Map</button>
+    </>
   )
 }
 
