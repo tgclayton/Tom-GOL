ea79854442920159c3b978dbe0b8d32cb7759ca9
Tom Clayton <tgclayton1@gmail.com>
2020-05-24 12:43:45 +1200

    setup component routing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 import Home from './Home'
+import GameView from './GameView'
+import Instructions from './Instructions'
+import LoadStart from './LoadStart'
 
 class App extends Component {
   render () {
@@ -8,6 +11,9 @@ class App extends Component {
       <Router>
         <h1 id = 'main-title'>The Game of Life</h1>
         <Route exact path = '/' component = {() => <Home/>} />
+        <Route exact path = '/game' component = {() => <GameView/>} />
+        <Route exact path = '/instructions' component = {() => <Instructions/>} />
+        <Route exact path = '/load' component = {() => <LoadStart/>} />
       </Router>
     )
   }
---
+++ client/components/GameView.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const GameView = () => {
+  return (
+    <div id='tile-container' className = ''>
+    </div>
+  )
+}
+
+export default GameView
