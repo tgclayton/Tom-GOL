378bb53b12a4f8320e93ecb5db6fb54b9d528478
Tom Clayton <tgclayton1@gmail.com>
2020-05-26 08:53:31 +1200

    make gameview class based

--- client/components/GameView.jsx
+++ client/components/GameView.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Tile from './Tile'
 import { makeRandomMap } from './functions'
 
-const GameView = (props) => {
-  return (
-    <div className = 'centerer'>
-      <div id='tile-container' className = ''>
-        {props.mapArr.map((tile, idx) => {
-          return (
-            <Tile key = {idx} value = {tile}/>
-          )
-        })}
+class GameView extends Component {
+
+  render () {
+    return (
+      <div className = 'centerer'>
+        <div id='tile-container' className = ''>
+          {this.props.mapArr.map((tile, idx) => {
+            return (
+              <Tile key = {idx} value = {tile}/>
+            )
+          })}
+        </div>
+        <p>Generation: {this.props.gen}</p>
+        <button onClick = {() => this.props.setMap(makeRandomMap())}>Create Random Map</button>
+        <button onClick = {() => this.props.runGame(this.props.mapArr)}>Run Game</button>
+        <button onClick = {() => this.props.stopGame(this.props.mapArr)}>Stop Game</button>
       </div>
-      <p>Generation: {props.gen}</p>
-      <button onClick = {() => props.setMap(makeRandomMap())}>Create Random Map</button>
-      <button onClick = {() => props.runGame(props.mapArr)}>Run Game</button>
-      <button onClick = {() => props.stopGame(props.mapArr)}>Stop Game</button>
-    </div>
-  )
+    )
+  }
 }
 
 export default GameView
