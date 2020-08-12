ea15850538432dd9c513c835e9074e02be98b8b2
Tom Clayton <tgclayton1@gmail.com>
2020-08-13 11:09:07 +1200

    once more

--- migrations/20200721162542_saves.js
+++ migrations/20200721162542_saves.js
@@ -1,8 +1,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable('Saves', function (table) {
     table.increments().primary()
-    table.string('name')
-    table.string('description')
+    table.string('name', 10000)
+    table.string('description', 10000)
     table.string('fieldData', 100000)
   })
 }
