0081ff76146003cace6c9478ef272ffc52070a72
Tom Clayton <tgclayton1@gmail.com>
2020-09-08 20:18:55 +1200

    add more text to home page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const Home = () => {
   return (
     <div className = 'page'>
-      <div>
+      <div id = 'landing-description'>
+        <p>
+          The Game of Life is a cellular automaton invented by mathematician John Conway in 1970.
 
+          The game consists of a field of square cells, nominally on an infinite grid but in this implementation
+          on a limited grid with edge wrapping, i.e. cells on opposite sides of the field border each other.
+        </p>
       </div>
     </div>
   )
